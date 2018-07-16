d56b1e48dd4fdde09112fd678eec9b3c7e3b0761
Zion Nimchuk <zionnimchuk@gmail.com>
2018-07-16 12:17:42 -0700

    Fix the things

--- src/main.rs
+++ src/main.rs
@@ -9,115 +9,70 @@ use piston::input::*;
 use glutin_window::GlutinWindow as Window;
 use opengl_graphics::{ GlGraphics, OpenGL };
 
-#[cfg(all(feature="winit", feature="glium"))] #[macro_use] extern crate conrod;
-
-fn main() {
-    feature::main();
+pub struct App {
+    gl: GlGraphics, // OpenGL drawing backend.
+    rotation: f64   // Rotation for the square.
 }
 
-#[cfg(all(feature="winit", feature="glium"))]
-mod feature {
-    use conrod::{self, widget, Colorable, Positionable, Widget};
-    use conrod::backend::glium::glium::{self, Surface};
+impl App {
+    fn render(&mut self, args: &RenderArgs) {
+        use graphics::*;
 
-    pub fn main() {
-        const WIDTH: u32 = 400;
-        const HEIGHT: u32 = 200;
+        const GREEN: [f32; 4] = [0.0, 1.0, 0.0, 1.0];
+        const RED:   [f32; 4] = [1.0, 0.0, 0.0, 1.0];
 
-        // Build the window.
-        let mut events_loop = glium::glutin::EventsLoop::new();
-        let window = glium::glutin::WindowBuilder::new()
-            .with_title("Hello Conrod!")
-            .with_dimensions(WIDTH, HEIGHT);
-        let context = glium::glutin::ContextBuilder::new()
-            .with_vsync(true)
-            .with_multisampling(4);
-        let display = glium::Display::new(window, context, &events_loop).unwrap();
+        let square = rectangle::square(0.0, 0.0, 50.0);
+        let rotation = self.rotation;
+        let (x, y) = ((args.width / 2) as f64,
+                      (args.height / 2) as f64);
 
-        // construct our `Ui`.
-        let mut ui = conrod::UiBuilder::new([WIDTH as f64, HEIGHT as f64]).build();
+        self.gl.draw(args.viewport(), |c, gl| {
+            // Clear the screen.
+            clear(GREEN, gl);
 
-        // Generate the widget identifiers.
-        widget_ids!(struct Ids { text });
-        let ids = Ids::new(ui.widget_id_generator());
+            let transform = c.transform.trans(x, y)
+                                       .rot_rad(rotation)
+                                       .trans(-25.0, -25.0);
 
-        // Add a `Font` to the `Ui`'s `font::Map` from file.
-        const FONT_PATH: &'static str =
-            concat!(env!("CARGO_MANIFEST_DIR"), "/assets/fonts/NotoSans/NotoSans-Regular.ttf");
-        ui.fonts.insert_from_file(FONT_PATH).unwrap();
+            // Draw a box rotating around the middle of the screen.
+            rectangle(RED, square, transform, gl);
+        });
+    }
 
-        // A type used for converting `conrod::render::Primitives` into `Command`s that can be used
-        // for drawing to the glium `Surface`.
-        let mut renderer = conrod::backend::glium::Renderer::new(&display).unwrap();
+    fn update(&mut self, args: &UpdateArgs) {
+        // Rotate 2 radians per second.
+        self.rotation += 2.0 * args.dt;
+    }
+}
 
-        // The image map describing each of our widget->image mappings (in our case, none).
-        let image_map = conrod::image::Map::<glium::texture::Texture2d>::new();
+fn main() {
+    // Change this to OpenGL::V2_1 if not working.
+    let opengl = OpenGL::V3_2;
 
-        let mut events = Vec::new();
+    // Create an Glutin window.
+    let mut window: Window = WindowSettings::new(
+            "spinning-square",
+            [200, 200]
+        )
+        .opengl(opengl)
+        .exit_on_esc(true)
+        .build()
+        .unwrap();
 
-        'render: loop {
-            events.clear();
+    // Create a new game and run it.
+    let mut app = App {
+        gl: GlGraphics::new(opengl),
+        rotation: 0.0
+    };
 
-            // Get all the new events since the last frame.
-            events_loop.poll_events(|event| { events.push(event); });
+    let mut events = Events::new(EventSettings::new());
+    while let Some(e) = events.next(&mut window) {
+        if let Some(r) = e.render_args() {
+            app.render(&r);
+        }
 
-            // If there are no new events, wait for one.
-            if events.is_empty() {
-                events_loop.run_forever(|event| {
-                    events.push(event);
-                    glium::glutin::ControlFlow::Break
-                });
-            }
-
-            // Process the events.
-            for event in events.drain(..) {
-
-                // Break from the loop upon `Escape` or closed window.
-                match event.clone() {
-                    glium::glutin::Event::WindowEvent { event, .. } => {
-                        match event {
-                            glium::glutin::WindowEvent::Closed |
-                            glium::glutin::WindowEvent::KeyboardInput {
-                                input: glium::glutin::KeyboardInput {
-                                    virtual_keycode: Some(glium::glutin::VirtualKeyCode::Escape),
-                                    ..
-                                },
-                                ..
-                            } => break 'render,
-                            _ => (),
-                        }
-                    }
-                    _ => (),
-                };
-
-                // Use the `winit` backend feature to convert the winit event to a conrod input.
-                let input = match conrod::backend::winit::convert_event(event, &display) {
-                    None => continue,
-                    Some(input) => input,
-                };
-
-                // Handle the input with the `Ui`.
-                ui.handle_event(input);
-
-                // Set the widgets.
-                let ui = &mut ui.set_widgets();
-
-                // "Hello World!" in the middle of the screen.
-                widget::Text::new("Hello World!")
-                    .middle_of(ui.window)
-                    .color(conrod::color::WHITE)
-                    .font_size(32)
-                    .set(ids.text, ui);
-            }
-
-            // Draw the `Ui` if it has changed.
-            if let Some(primitives) = ui.draw_if_changed() {
-                renderer.fill(&display, primitives, &image_map);
-                let mut target = display.draw();
-                target.clear_color(0.0, 0.0, 0.0, 1.0);
-                renderer.draw(&display, &mut target, &image_map).unwrap();
-                target.finish().unwrap();
-            }
+        if let Some(u) = e.update_args() {
+            app.update(&u);
         }
     }
 }
