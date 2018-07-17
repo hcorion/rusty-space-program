8d03d0464663292a116d3f0fcc2178bdf6353d24
bnmcgn@gmail.com <bnmcgn@gmail.com>
2018-07-17 13:10:42 -0700

    Cleaned up old rectangle drawing code.

--- src/main.rs
+++ src/main.rs
@@ -32,26 +32,8 @@ impl App {
     fn render(&mut self, args: &RenderArgs, event: Event) {
         use graphics::*;
 
-        const GREEN: [f32; 4] = [0.0, 1.0, 0.0, 1.0];
-        const RED:   [f32; 4] = [1.0, 0.0, 0.0, 1.0];
-
-        let square = rectangle::square(0.0, 0.0, 50.0);
-        let rotation = self.rotation;
-        let (x, y) = ((args.width / 2) as f64,
-                      (args.height / 2) as f64);
         self.draw_background(event.clone());
         self.draw_birds(event, 13, 0.5);
-        self.gl.draw(args.viewport(), |c, gl| {
-            // Clear the screen.
-            //clear(GREEN, gl);
-
-            let transform = c.transform.trans(x, y)
-                                       .rot_rad(rotation)
-                                       .trans(-25.0, -25.0);
-
-            // Draw a box rotating around the middle of the screen.
-            rectangle(RED, square, transform, gl);
-        });
     }
 
     fn update(&mut self, args: &UpdateArgs) {
